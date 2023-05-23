49a3ba7522d497b06f4164089d1bee82ec3b3b35
Liam310 <liam.duncan310@gmail.com>
2023-05-23 15:57:57 +0100

    remove playground

--- playground.sql
+++
@@ -1,9 +0,0 @@
-\c nc_snacks;
-
--- SELECT * FROM categories;
--- SELECT * FROM snacks;
-
-INSERT INTO snacks
-(snack_name, snack_description, price, category_id)
-VALUES
-('7up', 'it''s light!', 70, 3);
